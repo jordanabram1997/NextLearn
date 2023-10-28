f58b03469556ae1276b6b68e6b7214320bda06a1
JordanAbram1997 <jordan16abram@gmail.com>
2023-10-28 19:12:23 +0100

    Adding crud for invoices

--- app/ui/invoices/edit-form.tsx
+++ app/ui/invoices/edit-form.tsx
@@ -1,6 +1,8 @@
 'use client';
 
 import { CustomerField, InvoiceForm } from '@/app/lib/definitions';
+import { updateInvoice } from '@/app/lib/actions';
+
 import {
   CheckIcon,
   ClockIcon,
@@ -10,6 +12,7 @@ import {
 import Link from 'next/link';
 import { Button } from '@/app/ui/button';
 
+
 export default function EditInvoiceForm({
   invoice,
   customers,
@@ -18,7 +21,7 @@ export default function EditInvoiceForm({
   customers: CustomerField[];
 }) {
   return (
-    <form>
+    <form action={updateInvoice}>
       <div className="rounded-md bg-gray-50 p-4 md:p-6">
         {/* Invoice ID */}
         <input type="hidden" name="id" value={invoice.id} />
