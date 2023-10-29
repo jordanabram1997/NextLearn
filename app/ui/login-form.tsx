5927c8a08e95172f36dfc4624c49964ad6dab1e9
JordanAbram1997 <jordan16abram@gmail.com>
2023-10-29 15:13:01 +0000

    ...

--- app/ui/login-form.tsx
+++ app/ui/login-form.tsx
@@ -64,7 +64,7 @@ export default function LoginForm() {
         </div>
         <LoginButton />
         <div className="flex h-8 items-end space-x-1">
-          {code === 'CredentialsSignin' && (
+          {code === 'CredentialSignin' && (
             <>
               <ExclamationCircleIcon className="h-5 w-5 text-red-500" />
               <p aria-live="polite" className="text-sm text-red-500">
